dce9b459c7562eff646e36e447817562557bd92e
modernhousetech <modernhousetech@gmail.com>
2021-06-20 15:45:12 -0700

    Added _countof

--- include/app_defs_common.h
+++ include/app_defs_common.h
@@ -2,3 +2,6 @@
 // Documents APIs that are entry points into the app from
 // outaide the app (from mqtt, timers, sensors callback, etc.)
 #define _entry_point
+
+#define _countof(arr) sizeof(arr) / sizeof(arr[0])
+
